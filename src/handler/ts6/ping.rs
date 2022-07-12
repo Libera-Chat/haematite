3ba634dcc4b4057d18097b0a9974382ef309f128
Jess Porter <github@lolnerd.net>
2022-07-12 20:50:54 +0100

    handle KILL (#12)

--- src/handler/ts6/ping.rs
+++ src/handler/ts6/ping.rs
@@ -7,10 +7,11 @@ use super::TS6Handler;
 
 impl TS6Handler {
     pub fn handle_ping(network: &mut Network, line: &Line) -> Result<Outcome, &'static str> {
+        let me = &network.servers[&network.me];
         Ok(Outcome::Response(vec![format!(
             ":{} PONG {} {}",
-            network.me.sid,
-            network.me.name,
+            me.sid,
+            me.name,
             line.args.get(0).ok_or("missing argument")?.decode(),
         )]))
     }

75cde411f422700f237879a6fd63509685fc172e
Jess Porter <github@lolnerd.net>
2022-07-17 19:34:40 +0100

    tmode wants 3-or-more args (#29)

--- haematite_s2s/src/ts6/tmode.rs
+++ haematite_s2s/src/ts6/tmode.rs
@@ -7,8 +7,9 @@ use crate::util::DecodeHybrid;
 
 use super::parse_mode_args;
 
+//:420AAAAAB TMODE 1658071342 #test +bbb a!*@* b!*@* c!*@*
 pub fn handle(network: &mut Network, line: &Line) -> Result<Outcome, Error> {
-    Line::assert_arg_count(line, 3)?;
+    Line::assert_arg_count(line, 3..)?;
 
     let channel = network.get_channel_mut(&line.args[1])?;
     let modes = modes_from(&line.args[2].decode());

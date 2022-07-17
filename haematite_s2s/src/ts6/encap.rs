a4c3ae4f26ba094fdb6c244ad512fbebb6e71053
Jess Porter <github@lolnerd.net>
2022-07-17 20:39:27 +0100

    handle ENCAP SU (#26)

---
+++ haematite_s2s/src/ts6/encap.rs
@@ -0,0 +1,15 @@
+mod su;
+
+use haematite_models::network::Network;
+
+use crate::handler::{Error, Outcome};
+use crate::line::Line;
+
+pub fn handle(network: &mut Network, line: &Line) -> Result<Outcome, Error> {
+    Line::assert_arg_count(line, 2..)?;
+
+    match line.args[1].as_slice() {
+        b"SU" => su::handle(network, line),
+        _ => Ok(Outcome::Unhandled),
+    }
+}
---
+++ haematite_s2s/src/ts6/encap/su.rs
@@ -0,0 +1,17 @@
+use haematite_models::network::Network;
+
+use crate::handler::{Error, Outcome};
+use crate::line::Line;
+use crate::util::DecodeHybrid;
+
+//:00A ENCAP * SU :420AAAAAB
+//:00A ENCAP * SU 420AAAAAB :jess
+pub fn handle(network: &mut Network, line: &Line) -> Result<Outcome, Error> {
+    Line::assert_arg_count(line, 3..)?;
+
+    let uid = &line.args[2];
+    let user = network.get_user_mut(uid)?;
+    user.account.value = line.args.get(3).map(DecodeHybrid::decode);
+
+    Ok(Outcome::Empty)
+}

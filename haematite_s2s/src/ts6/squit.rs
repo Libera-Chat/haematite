e40039e671aada511e491bf3e4a13941513c53c3
jesopo <github@lolnerd.net>
2022-07-17 18:35:09 +0000

    SQUIT takes one-or-more args

--- haematite_s2s/src/ts6/squit.rs
+++ haematite_s2s/src/ts6/squit.rs
@@ -5,8 +5,9 @@ use crate::line::Line;
 
 use super::util::del_server;
 
+//SQUIT 00A :by jess:
 pub fn handle(network: &mut Network, line: &Line) -> Result<Outcome, Error> {
-    Line::assert_arg_count(line, 1)?;
+    Line::assert_arg_count(line, 1..)?;
 
     let sid = &line.args[0];
     del_server(network, sid)?;

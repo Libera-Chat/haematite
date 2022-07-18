23034c0bb4759cb4d4f0866ba95cebb91c1257a4
Jess Porter <github@lolnerd.net>
2022-07-18 01:18:59 +0100

    handle sjoin collision correctly (#28)

--- haematite_s2s/src/ts6/sjoin.rs
+++ haematite_s2s/src/ts6/sjoin.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 use haematite_models::channel::{Channel, Membership};
-use haematite_models::network::Network;
+use haematite_models::network::{Error as StateError, Network};
 
 use crate::handler::{Error, Outcome};
 use crate::line::Line;
@@ -11,23 +11,38 @@ use crate::util::DecodeHybrid;
 use super::parse_mode_args;
 use super::util::{add_channel, add_user_channel};
 
+//:00A SJOIN 1658071435 #services +nst :@00AAAAAAB
 pub fn handle(network: &mut Network, line: &Line) -> Result<Outcome, Error> {
     Line::assert_arg_count(line, 4)?;
 
     let channel_name = &line.args[1];
     let uids = line.args[line.args.len() - 1]
         .split(|c| c == &b' ')
+        // we may get an empty last param
+        .filter(|a| !a.is_empty())
         .collect::<Vec<&[u8]>>();
 
-    let mut channel = Channel::new();
+    let channel_new = Channel::new();
+
+    let channel = match add_channel(network, channel_name.clone(), channel_new) {
+        Err(StateError::OverwrittenChannel) => {
+            // SJOIN caused by a netjoin elsewhere that's overwriting this channel
+            let channel = network.get_channel_mut(channel_name)?;
+            channel.modes.clear();
+            channel.mode_lists.clear();
+            channel
+        }
+        Err(_) => {
+            return Err(Error::InvalidState);
+        }
+        Ok(_) => network.get_channel_mut(channel_name)?,
+    };
 
     let modes = modes_from(&line.args[2].decode());
     let mode_args = line.args[3..line.args.len() - 1].iter();
     for (mode, _, arg) in parse_mode_args(modes, mode_args) {
         channel.modes.insert(mode, arg.map(DecodeHybrid::decode));
     }
-
-    add_channel(network, channel_name.clone(), channel)?;
 
     for uid in uids {
         //TODO: precompile

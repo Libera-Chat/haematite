422351f7ded188c92838a2e77c0d53dfe6dea5f0
jesopo <github@lolnerd.net>
2022-07-26 23:34:36 +0000

    first draft of permissions-based api output

--- haematite_api/src/lib.rs
+++ haematite_api/src/lib.rs
@@ -1,4 +1,7 @@
+use std::collections::HashSet;
+
 use haematite_models::network::Network;
+use serde_json::{Map, Value};
 
 pub struct Api {}
 
@@ -8,7 +11,41 @@ pub enum Error {
 }
 
 impl Api {
+    fn prune<'a>(path: &'a str, paths: &HashSet<&'a str>, value: Value) -> Option<(bool, Value)> {
+        match value {
+            Value::Object(map_old) => {
+                let mut map_new = Map::new();
+
+                for (key, value) in map_old.into_iter() {
+                    let path = format!("{}/{}", path, key);
+                    if let Some((external_vertex, value)) = Api::prune(&path, paths, value) {
+                        if !external_vertex || paths.contains(path.as_str()) {
+                            map_new.insert(key, value);
+                        }
+                    }
+                }
+
+                if map_new.is_empty() {
+                    None
+                } else {
+                    let output = (false, Value::Object(map_new));
+                    Some(output)
+                }
+            }
+            _ => {
+                let output = (true, value);
+                Some(output)
+            }
+        }
+    }
+
     pub fn get_network(network: &Network) -> Result<String, serde_json::Error> {
-        serde_json::to_string(network)
+        let paths = HashSet::from(["/users/00AAAAAAG/nick"]);
+
+        if let Some((_, value)) = Api::prune("", &paths, serde_json::to_value(network)?) {
+            Ok(value.to_string())
+        } else {
+            Ok("{}".to_string())
+        }
     }
 }

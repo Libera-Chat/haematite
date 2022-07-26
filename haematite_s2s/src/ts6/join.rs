783d754fae7eee24e96e2e5daf4ea041de757b78
Jess Porter <github@lolnerd.net>
2022-07-26 15:22:54 +0100

    JOIN expects 3 args, not 2 (#41)

--- haematite_s2s/src/ts6/join.rs
+++ haematite_s2s/src/ts6/join.rs
@@ -8,7 +8,7 @@ use super::util::state::add_user_channel;
 
 //:420AAAABG JOIN 1657651885 #test +
 pub fn handle(network: &mut Network, line: &Line) -> Result<Outcome, Error> {
-    Line::assert_arg_count(line, 2)?;
+    Line::assert_arg_count(line, 3)?;
 
     let uid = line.source.as_ref().ok_or(Error::MissingSource)?;
     let channel = &line.args[1];

4d2659432815dc61c87225b09d924a25a5bfc3eb
Jess Porter <github@lolnerd.net>
2022-11-05 22:47:55 +0000

    implement a permission-aware proxy serializer (#56)

---
+++ haematite_ser/src/error.rs
@@ -0,0 +1,29 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+pub enum Error {
+    Message(String),
+    KeyMustBeString,
+    Serde(serde_json::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
+impl serde::ser::Error for Error {
+    fn custom<T: Display>(msg: T) -> Self {
+        Self::Message(msg.to_string())
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Self::Serde(e)
+    }
+}
---
+++ haematite_ser/src/serialize_struct.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+
+use crate::error::{Error, Result};
+use crate::wrap::{SerializeWrap, WrapType};
+use crate::Serializer;
+
+use serde::ser::Serialize;
+
+pub struct SerializeStruct {
+    name: &'static str,
+    map: HashMap<&'static str, SerializeWrap>,
+}
+
+impl SerializeStruct {
+    pub fn new(name: &'static str, len: usize) -> Self {
+        Self {
+            name,
+            map: HashMap::with_capacity(len),
+        }
+    }
+}
+
+impl serde::ser::SerializeStruct for SerializeStruct {
+    type Ok = WrapType;
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        let mut serializer = Serializer {};
+        self.map
+            .insert(key, SerializeWrap::new(value.serialize(&mut serializer)?));
+        Ok(())
+    }
+    fn end(self) -> Result<Self::Ok> {
+        Ok(Self::Ok::Struct(self.name, self.map))
+    }
+}

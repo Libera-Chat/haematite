7ed8aada81b2fea07b5f4a784d9e823528ac2257
jesopo <github@lolnerd.net>
2022-11-06 19:13:46 +0000

    we don't need to use serde_json::Value anymore

--- haematite_api/src/lib.rs
+++ haematite_api/src/lib.rs
@@ -2,7 +2,8 @@ use std::sync::{Arc, PoisonError, RwLock};
 
 use haematite_models::irc::network::Network;
 use haematite_models::meta::user::User;
-use serde_json::{Error as JsonError, Value};
+use serde::Serialize;
+use serde_json::Error as JsonError;
 
 pub enum Format {
     Terse,
@@ -38,23 +39,21 @@ impl Api {
         Self { network, format }
     }
 
-    fn format(&self, value: Value) -> Result<String, JsonError> {
+    fn format<T: Serialize>(&self, value: &T) -> Result<String, JsonError> {
         Ok(match self.format {
-            Format::Terse => serde_json::to_string(&value)?,
-            Format::Pretty => serde_json::to_string_pretty(&value)?,
+            Format::Terse => serde_json::to_string(value)?,
+            Format::Pretty => serde_json::to_string_pretty(value)?,
         })
     }
 
     pub fn get_network(&self, _user: &User) -> Result<String, Error> {
         let network = self.network.read()?;
-        let value = serde_json::to_value(&*network)?;
-        Ok(self.format(value)?)
+        Ok(self.format(&*network)?)
     }
 
     pub fn get_user(&self, _user: &User, uid: &str) -> Result<String, Error> {
         let network = self.network.read()?;
         let user = network.users.get(uid).ok_or(Error::Argument)?;
-        let value = serde_json::to_value(&user)?;
-        Ok(self.format(value)?)
+        Ok(self.format(user)?)
     }
 }
